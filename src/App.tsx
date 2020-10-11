60244333be923a77723edbf16270256188f92ed6
David Giraldo <davidgiraldobueno@gmail.com>
2020-10-11 18:47:41 -0500

    Default Overlay

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import Page from './components/Page';
+
+import { GlobalStyles } from './styles/GlobalStyles';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <>
+     <Page />
+     <GlobalStyles />
+    </>
   );
 }
 
---
+++ src/components/Page/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container } from './styles';
+import DefaultOverlayContent from '../DefaultOverlayContent';
+
+const Page: React.FC = () => {
+  return (
+    <Container>
+      
+      <h1>Page</h1>
+
+    </Container>
+  );
+};
+
+export default Page;
