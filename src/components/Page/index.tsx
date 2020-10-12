bb64940e0d9bf0ddb4345fe42c872bdcb57d4096
David Giraldo <davidgiraldobueno@gmail.com>
2020-10-12 10:39:33 -0500

    adv 1

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React from 'react'
 
-import { Container } from './styles';
-import DefaultOverlayContent from '../DefaultOverlayContent';
+import { ModelsWrapper, ModelSection } from '../Model'
+import DefaultOverlayContent from '../DefaultOverlayContent'
+
+import { Container } from './styles'
 
 const Page: React.FC = () => {
   return (
     <Container>
-      
-      <h1>Page</h1>
+      <ModelsWrapper>
+        <div>
+          {[
+            'Model One',
+            'Model Two',
+            'Model Three',
+            'Model Four',
+            'Model Five',
+            'Model Six',
+            'Model Seven'
+          ].map(modelName => (
+            <ModelSection
+              key={modelName}
+              className="colored"
+              modelName={modelName}
+              overlayNode={
+                <DefaultOverlayContent
+                  label={modelName}
+                  description="Order Online for Delivery"
+                />
+              }
+            />
+          ))}
+        </div>
 
+      </ModelsWrapper>
     </Container>
-  );
-};
+  )
+}
 
-export default Page;
+export default Page
